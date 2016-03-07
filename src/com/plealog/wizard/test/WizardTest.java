ec6805c04b2afd980e799673c1ab3e4fee20d9bc
pgdurand <patrick@plealog.com>
2016-03-07 20:01:02 +0100

    minor doc update

--- src/com/plealog/wizard/test/WizardTest.java
+++ src/com/plealog/wizard/test/WizardTest.java
@@ -1,3 +1,17 @@
+/* Copyright (C) 2003-2016 Patrick G. Durand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.plealog.wizard.test;
 
 import java.awt.FlowLayout;
@@ -26,7 +40,7 @@ public class WizardTest {
     frame = new JFrame();
 
     frame.setTitle("Wizard Test");
-    frame.setSize(1200, 650);
+    frame.setSize(250, 150);
     frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setLayout(new FlowLayout());
--- src/com/plealog/wizard/ui/WizardDialog.java
+++ src/com/plealog/wizard/ui/WizardDialog.java
@@ -1,3 +1,17 @@
+/* Copyright (C) 2003-2016 Patrick G. Durand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.plealog.wizard.ui;
 
 import java.awt.BorderLayout;
@@ -46,8 +60,8 @@ public class WizardDialog extends JDialog {
   private JButton							finishButton;
   private JButton							cancelButton;
 
-  private static final int				DIALOG_WIDTH		= 900;
-  private static final int				DIALOG_HEIGHT		= 520;
+  private static final int				DIALOG_WIDTH		= 640;
+  private static final int				DIALOG_HEIGHT		= 350;
 
 
   private int								idx_gbc_stepPanel	= 0;
