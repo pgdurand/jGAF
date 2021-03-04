baf02415947c8dae8d865cd4eab858b22b266b49
Patrick Durand <patrick@plealog.com>
2021-03-04 15:59:06 +0100

    fix closing application for macOS

--- src/com/plealog/genericapp/ui/menu/EZActionManager.java
+++ src/com/plealog/genericapp/ui/menu/EZActionManager.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2003-2016 Patrick G. Durand
+/* Copyright (C) 2003-2021 Patrick G. Durand
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -643,12 +643,7 @@ public class EZActionManager implements PropertyChangeListener {
         }
       }
 
-      //for MacOs X, let the AppleCOnnector do the job
-      if (EZEnvironmentImplem.getOSType()==EZEnvironment.MAC_OS){
-        return bRet;
-      }
-
-      //for all other systems, do the System.exit() if needed
+      //do the System.exit() if needed
       if(bRet){
         System.exit(0);
       }
